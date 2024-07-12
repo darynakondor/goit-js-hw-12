0806f99178fe081f4604e09a3b16615f57fb525e
daryna <104278299+darynakondor@users.noreply.github.com>
2024-07-12 15:32:31 +0300

    update

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -33,3 +33,47 @@ export function renderPhotosList(photos) {
     `);
     gallery.insertAdjacentHTML("beforeend", html);
 }
+
+function saveLanguageCode(langugageCode) {
+    localStorage.setItem('language', langugageCode)
+}
+
+function getLanguageCode() {
+     return localStorage.getItem('language') || 'en'
+}
+  
+function loadTranslations(lang) {
+  return fetch(`./languages/${lang}.json`)
+    .then(response => response.json())
+    .catch(error => console.error('Error loading translations:', error));
+}
+  
+function translatePage(translations) {
+  document.querySelectorAll('[data-translate]').forEach(element => {
+    const key = element.getAttribute('data-translate');
+    if (element.tagName === 'INPUT' && element.placeholder === 'comments') {
+      element.placeholder = translations[key] || element.placeholder;
+    } else {
+      element.innerHTML = translations[key] || element.textContent;
+    }
+  });
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const languageLinks = document.querySelectorAll('.languages .link')
+
+  const savedLang = getLanguageCode();
+  document.documentElement.lang = savedLang;
+
+  loadTranslations(savedLang).then(translations => {
+    translatePage(translations);
+  });
+
+  languageLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const selectedLang = event.target.getAttribute('data-lang');
+      saveLanguageCode(selectedLang);
+      window.location.href = `/${selectedLang}`;
+    });
+  });
+});
